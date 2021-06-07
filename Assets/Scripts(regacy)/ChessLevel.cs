ae930e704a94c707db766828e3bf6ee2e0811022
Tatsuhiro Yamashita <taabox.app@gmail.com>
2021-06-08 01:25:45 +0900

    不要アセット整理

--- Assets/Scripts(regacy)/ChessLevel.cs
+++
@@ -1,6 +0,0 @@
-﻿
-
-public enum ChessLevel
-{
-	Beginner, Regular, Pro
-}
--- Assets/Scripts(regacy)/Tweeners/IObjectTweener.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-internal interface IObjectTweener
-{
-	void MoveTo(Transform transform, Vector3 targetPosition);
-}
